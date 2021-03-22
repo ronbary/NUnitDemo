0ce9cd49664d41be6a628f42c6003e174dad53b9
ronbary <ronibary@gmail.com>
2021-03-22 14:39:28 +0200

    Add project files.

---
+++ CheckPalindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitDemo
+{
+    class CheckPalindrome
+    {
+
+
+        public bool IsPalindrom(string input)
+        {
+            bool result = false;
+
+            char[] inputArray = input.ToCharArray();
+
+            Array.Reverse(inputArray);
+
+            string strReversed = new string(inputArray);
+
+            result = strReversed.Equals(input, StringComparison.OrdinalIgnoreCase);
+
+            return result;
+        }
+
+
+    }
+}
---
+++ PalindromeTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitDemo
+{
+    [TestFixture]
+    class PalindromeTest
+    {
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void isPalindrome_ValidString_ReturnTrue()
+        {
+            CheckPalindrome obj = new CheckPalindrome();
+
+            Assert.IsTrue( obj.IsPalindrom("abcba"));
+
+        
+        }
+
+
+        [Test]
+        public void isPalindrome_InvalidString_ReturnFalse()
+        {
+            CheckPalindrome obj = new CheckPalindrome();
+
+            Assert.IsFalse(obj.IsPalindrom("agcba"));
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+
+
+
+    }
+}
